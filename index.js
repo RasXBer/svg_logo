316642d8ca8db883811cba6d9adc38d004208bcb
rasika <rasikab@yahoo.com>
2024-04-29 02:09:13 +1000

    corrected text and shape color

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ y=145;
 }
 const finalSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
   ${svgElement}
-<text x= "${x}" y="${y}" fill = "${textColor}" font-size="50" text-anchor="middle">${text}</text>
+<text x= "${x}" y="${y}" fill=${textColor} font-size="50" text-anchor="middle">${text}</text>
 </svg>`;
 
 fs.writeFileSync('logo.svg', finalSvg);
